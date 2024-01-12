7c915cde502392296b78afc193899c79ba9a725e
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-01-12 00:51:24 -0300

    Tornando genericos filas push e pull e tarefas cron

--- src/main/java/com/ccp/jn/cron/controller/JnCronTasksController.java
+++ src/main/java/com/ccp/jn/cron/controller/JnCronTasksController.java
@@ -1,8 +1,5 @@
 package com.ccp.jn.cron.controller;
 
-import java.util.function.Function;
-
-import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.dependency.injection.CcpDependencyInjection;
 import com.ccp.implementations.db.bulk.elasticsearch.CcpElasticSerchDbBulk;
 import com.ccp.implementations.db.dao.elasticsearch.CcpElasticSearchDao;
@@ -13,10 +10,8 @@ import com.ccp.implementations.file.bucket.gcp.CcpGcpFileBucket;
 import com.ccp.implementations.http.apache.mime.CcpApacheMimeHttp;
 import com.ccp.implementations.instant.messenger.telegram.CcpTelegramInstantMessenger;
 import com.ccp.implementations.json.gson.CcpGsonJsonHandler;
-import com.ccp.jn.cron.tasks.JnGenericCronTask;
 
 public class JnCronTasksController {
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
 		CcpDependencyInjection.loadAllDependencies
 		(
@@ -30,11 +25,7 @@ public class JnCronTasksController {
 				new CcpElasticSerchDbBulk(),
 				new CcpElasticSearchDao()
 		);
-		String taskName = args[0];
-		Class<Function<CcpJsonRepresentation, CcpJsonRepresentation>> forName = (Class<Function<CcpJsonRepresentation, CcpJsonRepresentation>>) Class.forName(JnGenericCronTask.class.getPackage().getName() + "." + taskName);
-		Function<CcpJsonRepresentation, CcpJsonRepresentation> injected = forName.getDeclaredConstructor().newInstance();
-		String parameters = args[1];
-		CcpJsonRepresentation mdParameters = new CcpJsonRepresentation(parameters);
-		injected.apply(mdParameters);
+		
+		CcpCronTasksController.main(args);
 	}
 }
--- src/main/java/com/ccp/jn/cron/tasks/JnCronTaskGroupedViewsByRecruiter.java
+++
@@ -1,5 +0,0 @@
-package com.ccp.jn.cron.tasks;
-
-public class JnCronTaskGroupedViewsByRecruiter {
-
-}
